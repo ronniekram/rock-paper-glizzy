aec4f9a039b933ecb04f1115f2546e2d2dbf4748
Arynn Boniface <arynn.boniface@gmail.com>
2022-03-22 21:19:07 -0400

    feat: added fonts

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,25 @@
 import * as React from "react";
 import { GlobalStyles } from "twin.macro";
+import { Global, css } from "@emotion/react";
+
+const fonts = css`
+	@font-face {
+		font-family: "Barlow Semi Condensed";
+		src: url("../assets/fonts/BarlowSemiCondensed-600.ttf")
+			format("ttf");
+		font-style: normal;
+		font-weight: 600;
+		font-display: fallback;
+	}
+	@font-face {
+		font-family: "Barlow Semi Condensed";
+		src: url("../assets/fonts/BarlowSemiCondensed-700.ttf")
+			format("ttf");
+		font-style: normal;
+		font-weight: 700;
+		font-display: fallback;
+	}
+`;
 
 const Layout = ({
 	children,
@@ -9,6 +29,7 @@ const Layout = ({
 }): JSX.Element => (
 	<div {...rest}>
 		<GlobalStyles />
+		<Global styles={fonts} />
 		{children}
 	</div>
 );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import Layout from "../components/Layout";
-import SEO from "../components/SEO";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 import "twin.macro";
 
 const Home = (): JSX.Element => {
 	return (
 		<Layout>
 			<SEO />
-			<div tw="text-2xl text-red-400 m-10">Hello world!</div>
+			<div tw="text-2xl m-10">Hello world!</div>
 		</Layout>
 	);
 };
