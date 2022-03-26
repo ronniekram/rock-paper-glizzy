ff8ed6767ee146280478527aeb1376f6cca2c4b2
Arynn Boniface <arynn.boniface@gmail.com>
2022-03-26 11:18:43 -0400

    feat: header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useContext } from "react";
 import tw, { styled } from "twin.macro";
 import Score from "./score";
+import { GameContext } from "../context/game-context";
+import logo from "../assets/images/logo.svg";
+import logoBonus from "../assets/images/logo-bonus.svg";
 
-// 311 x 96
-//
+const Wrapper = styled.div`
+	border: 3px solid rgba(255, 255, 255, 0.289199);
+	${tw`border-radius[5px]`};
+	${tw`p-3`}
+	${tw`width[19.4375] height[6rem]`};
+	${tw`lg:(width[46.875rem] height[9.375rem])`};
+	${tw`flex items-center justify-between`};
+`;
 
-const Header = () => {};
+const image = [tw`h-full`];
+
+const Header = () => {
+	const { game } = useContext(GameContext);
+
+	return (
+		<Wrapper>
+			<div>
+				{game === `RPS` ? (
+					<img
+						src={logo}
+						alt="Rock Paper Scissors"
+						css={[image]}
+					/>
+				) : (
+					<img
+						src={logoBonus}
+						alt="Rock Paper Scissors Lizard Spock"
+						css={[image]}
+					/>
+				)}
+			</div>
+			<Score />
+		</Wrapper>
+	);
+};
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,43 @@
 module.exports = {
-  theme: {
-    colors: {
-      scissor: {
-        100: `hsl(39, 89%, 49%)`,
-        200: `hsl(40, 84%, 53%)`,
-      },
-      paper: {
-        100: `hsl(230, 89%, 62%)`,
-        200: `hsl(230, 89%, 65%)`,
-      },
-      rock: {
-        100: `hsl(349, 71%, 52%)`,
-        200: `hsl(349, 70%, 56%)`,
-      },
-      lizard: {
-        100: `hsl(261, 73%, 60%) `,
-        200: `hsl(261, 72%, 63%)`,
-      },
-      spock: {
-        100: `hsl(189, 59%, 53%)`,
-        200: `hsl(189, 58%, 57%)`,
-      },
-      radial: {
-        100: `hsl(214, 47%, 23%)`,
-        200: `hsl(237, 49%, 15%)`,
-      },
-      copy: {
-        dark: `hsl(229, 25%, 31%)`,
-        score: `hsl(229, 64%, 46%)`,
-        header: `hsl(217, 16%, 45%)`,
-      }
-    },
-    fontFamily: {
-      display: [`Barlow Semi Condensed`, `sans-serif`],
-    },
-    fontWeight: {
-      regular: 600,
-      bold: 700,
-    },
-  },
-}
+	theme: {
+		colors: {
+			scissor: {
+				100: `hsl(39, 89%, 49%)`,
+				200: `hsl(40, 84%, 53%)`,
+			},
+			paper: {
+				100: `hsl(230, 89%, 62%)`,
+				200: `hsl(230, 89%, 65%)`,
+			},
+			rock: {
+				100: `hsl(349, 71%, 52%)`,
+				200: `hsl(349, 70%, 56%)`,
+			},
+			lizard: {
+				100: `hsl(261, 73%, 60%) `,
+				200: `hsl(261, 72%, 63%)`,
+			},
+			spock: {
+				100: `hsl(189, 59%, 53%)`,
+				200: `hsl(189, 58%, 57%)`,
+			},
+			radial: {
+				100: `hsl(214, 47%, 23%)`,
+				200: `hsl(237, 49%, 15%)`,
+			},
+			copy: {
+				dark: `hsl(229, 25%, 31%)`,
+				score: `hsl(229, 64%, 46%)`,
+				header: `hsl(217, 16%, 45%)`,
+			},
+			white: `#FFFFFF`,
+		},
+		fontFamily: {
+			display: [`Barlow Semi Condensed`, `sans-serif`],
+		},
+		fontWeight: {
+			regular: 600,
+			bold: 700,
+		},
+	},
+};
